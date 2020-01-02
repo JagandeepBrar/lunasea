cf676ee6417e6deafa0603de43ea966946b96fdf
Jagandeep Brar <me@jagandeepbrar.io>
2020-01-01 22:17:14 -0600

    [Settings, SABnzbd] Ability to save and test separately, hide FAB on connection error (#24)
    Closes #23 
    Closes #17

--- lib/pages/settings/subpages/clients/tabs/sabnzbd.dart
+++ lib/pages/settings/subpages/clients/tabs/sabnzbd.dart
@@ -45,16 +45,11 @@ class _SABnzbdState extends State<StatefulWidget> {
     Widget _buildFloatingActionButton() {
         return FloatingActionButton(
             heroTag: null,
-            tooltip: 'Test & Save',
+            tooltip: 'Save Settings',
             child: Elements.getIcon(Icons.save),
             onPressed: () async {
-                if(await SABnzbdAPI.testConnection(_sabnzbdValues)) {
-                    await Values.setSabnzbd(_sabnzbdValues);
-                    _refreshData();
-                    Notifications.showSnackBar(_scaffoldKey, 'Settings saved');
-                } else {
-                    Notifications.showSnackBar(_scaffoldKey, 'Connection test failed: Settings not saved');
-                }
+                await Values.setSabnzbd(_sabnzbdValues);
+                Notifications.showSnackBar(_scaffoldKey, 'Settings saved');
             },
         );
     }
@@ -118,6 +113,13 @@ class _SABnzbdState extends State<StatefulWidget> {
                         margin: Elements.getCardMargin(),
                         elevation: 4.0,
                     ),
+                    Elements.getButton('Test Connection', () async {
+                        if(await SABnzbdAPI.testConnection(_sabnzbdValues)) {
+                            Notifications.showSnackBar(_scaffoldKey, 'Connected successfully!');
+                        } else {
+                            Notifications.showSnackBar(_scaffoldKey, 'Connection test failed');
+                        }
+                    }),
                 ],
                 padding: Elements.getListViewPadding(),
             ),

f609baa705e5948b0354bdafcd0e194649ccc49d
Jagandeep Brar <jagandeep.brar@solarskyrise.com>
2020-01-31 11:35:24 -0600

    [Profiles] Ability to rename a profile

--- lib/configuration/profiles.dart
+++ lib/configuration/profiles.dart
@@ -15,6 +15,13 @@ class Profiles {
         profileList = profiles;
     }
 
+    static Future<bool> profileExists(String name) async {
+        final prefs = await SharedPreferences.getInstance();
+        profileList = prefs.getStringList('profiles');
+        if(profileList.contains(name)) return true;
+        return false;
+    }
+
     static Future<void> createProfile(String name) async {
         final prefs = await SharedPreferences.getInstance();
         profileList = prefs.getStringList('profiles');
@@ -36,6 +43,31 @@ class Profiles {
         prefs.remove('${name}_sabnzbd');
         prefs.remove('${name}_nzbget');
     }
+
+    static Future<void> renameProfile(String oldName, String newName) async {
+        final prefs = await SharedPreferences.getInstance();
+        //Set profile list, enabled_profile
+        profileList.add(newName);
+        prefs.setStringList('profiles', profileList);
+        if(enabledProfile == oldName) {
+            enabledProfile = newName;
+            prefs.setString('enabled_profile', enabledProfile);
+        }
+        //Set Automation
+        List<String> _sonarr = prefs.getStringList('${oldName}_sonarr');
+        List<String> _radarr = prefs.getStringList('${oldName}_radarr');
+        List<String> _lidarr = prefs.getStringList('${oldName}_lidarr');
+        prefs.setStringList('${newName}_sonarr', _sonarr);
+        prefs.setStringList('${newName}_radarr', _radarr);
+        prefs.setStringList('${newName}_lidarr', _lidarr);
+        //Set Clients
+        List<String> _nzbget = prefs.getStringList('${oldName}_nzbget');
+        List<String> _sabnzbd = prefs.getStringList('${oldName}_sabnzbd');
+        prefs.setStringList('${newName}_nzbget', _nzbget);
+        prefs.setStringList('${newName}_sabnzbd', _sabnzbd);
+        //Finally delete the old profile
+        await deleteProfile(oldName);
+    } 
 
     static Future<void> setProfile(String name) async {
         final prefs = await SharedPreferences.getInstance();

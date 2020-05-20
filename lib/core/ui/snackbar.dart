aef6b77ef0e06f08a2ead115b22c0afeadf12f72
Jagandeep Brar <jagandeep.brar@solarskyrise.com>
2020-05-20 14:31:32 -0500

    [Snackbar] Fixed cases where iOS swipe-back would lock UI

--- lib/core/ui/snackbar.dart
+++ lib/core/ui/snackbar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flushbar/flushbar.dart';
+import 'package:flash/flash.dart';
 import 'package:lunasea/core.dart';
 
 enum SNACKBAR_TYPE {
@@ -14,7 +14,7 @@ const _LEFT_PADDING = 8.0;
 Future<void> LSSnackBar({
     @required BuildContext context,
     @required String message,
-    String title,
+    @required String title,
     Duration duration,
     SNACKBAR_TYPE type = SNACKBAR_TYPE.info,
     bool showButton = false,
@@ -28,58 +28,58 @@ Future<void> LSSnackBar({
         case SNACKBAR_TYPE.success: color = LSColors.accent; icon = Icons.check_circle_outline; break;
         case SNACKBAR_TYPE.info: color = LSColors.blue; icon = Icons.info_outline; break;
     }
-    Flushbar(
-        titleText: Padding(
-            child: Text(
-                title,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18.0,
-                ),
-            ),
-            padding: EdgeInsets.only(left: _LEFT_PADDING),
-        ),
-        boxShadows: [BoxShadow(blurRadius: 6.0, spreadRadius: 4.0, color: Colors.black.withOpacity(0.10))],
-        messageText: Padding(
-            child: Text(
-                message,
-                style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 14.0,
-                ),
-            ),
-            padding: EdgeInsets.only(left: _LEFT_PADDING),
-        ),
-        //message: message,
+    showFlash(
+        context: context,
         duration: duration != null
             ? duration
             : showButton
                 ? Duration(seconds: 4)
                 : Duration(seconds: 2),
-        flushbarStyle: FlushbarStyle.FLOATING,
-        margin: EdgeInsets.all(8),
-        borderRadius: 8,
-        icon: Padding(
-            child: LSIconButton(
-                icon: icon,
-                color: color,
-            ),
-            padding: EdgeInsets.only(left: _LEFT_PADDING),
-        ),
-        mainButton: showButton
-            ? FlatButton(
-                child: Text(
-                    buttonText.toUpperCase(),
+        builder: (context, controller) => Flash(
+            backgroundColor: LSColors.primary,
+            controller: controller,
+            style: FlashStyle.floating,
+            boxShadows: [BoxShadow(blurRadius: 6.0, spreadRadius: 4.0, color: Colors.black.withOpacity(0.10))],
+            horizontalDismissDirection: HorizontalDismissDirection.horizontal,
+            margin: EdgeInsets.all(8.0),
+            borderRadius: BorderRadius.circular(10.0),
+            child: FlashBar(
+                title: Text(
+                    title,
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
                     ),
                 ),
-                textColor: LSColors.accent,
-                onPressed: buttonOnPressed,
-            )
-            : null,
-        animationDuration: Duration(milliseconds: 375),
-        backgroundColor: Theme.of(context).primaryColor,
-    )..show(context);
+                message: Text(
+                    message,
+                    style: TextStyle(
+                        color: Colors.white70,
+                    ),
+                ),
+                icon: Padding(
+                    child: LSIconButton(
+                        icon: icon,
+                        color: color,
+                    ),
+                    padding: EdgeInsets.only(left: _LEFT_PADDING),
+                ),
+                primaryAction: showButton
+                    ? FlatButton(
+                        child: Text(
+                            buttonText.toUpperCase(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                            ),
+                        ),
+                        textColor: LSColors.accent,
+                        onPressed: () {
+                            controller.dismiss();
+                            buttonOnPressed();
+                        }
+                    )
+                    : null,
+            ),
+        ),
+    );
 }

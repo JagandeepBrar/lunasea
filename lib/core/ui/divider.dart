97cfb2cbb94de2e381b1bc6068a9b540818be5dd
Jagandeep Brar <jagandeep.brar@solarskyrise.com>
2020-06-16 11:52:22 -0500

    [UI/Divider] Darken colour, slim down

--- lib/core/ui/divider.dart
+++ lib/core/ui/divider.dart
@@ -4,7 +4,7 @@ import 'package:lunasea/core.dart';
 class LSDivider extends StatelessWidget {
     @override
     Widget build(BuildContext context) => Divider(
-        thickness: 2.0,
-        color: LSColors.accent.withOpacity(0.25),
+        thickness: 1.0,
+        color: LSColors.splash,
     );
 }

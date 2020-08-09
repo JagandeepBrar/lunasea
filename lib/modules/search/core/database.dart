27997003d3adac93d357dbdd0ce65b8206b48a62
Jagandeep Brar <jagandeep.brar@solarskyrise.com>
2020-08-09 17:25:05 -0500

    [Search] Option to hide adult/xxx category

--- lib/modules/search/core/database.dart
+++ lib/modules/search/core/database.dart
@@ -8,12 +8,14 @@ class SearchDatabase {
 
 enum SearchDatabaseValue {
     NAVIGATION_INDEX,
+    HIDE_XXX,
 }
 
 extension SearchDatabaseValueExtension on SearchDatabaseValue {
     String get key {
         switch(this) {
             case SearchDatabaseValue.NAVIGATION_INDEX: return 'SEARCH_NAVIGATION_INDEX';
+            case SearchDatabaseValue.HIDE_XXX: return 'SEARCH_HIDE_XXX';
         }
         throw Exception('key not found'); 
     }
@@ -22,7 +24,10 @@ extension SearchDatabaseValueExtension on SearchDatabaseValue {
         final _box = Database.lunaSeaBox;
         switch(this) {
             case SearchDatabaseValue.NAVIGATION_INDEX: return _box.get(this.key, defaultValue: 0);
+            case SearchDatabaseValue.HIDE_XXX: return _box.get(this.key, defaultValue: false);
         }
         throw Exception('data not found'); 
     }
+
+    void put(dynamic value) => Database.lunaSeaBox.put(this.key, value);
 }

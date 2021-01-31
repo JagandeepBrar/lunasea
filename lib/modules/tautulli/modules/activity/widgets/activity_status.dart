a8fc987f105177162c40f6b0441f39e2df88f028
Jagandeep Brar <me@jagandeepbrar.io>
2021-01-30 19:37:35 -0600

    [Tautulli] Show session type and bandwidth type breakdown on activity page

--- lib/modules/tautulli/modules/activity/widgets/activity_status.dart
+++ lib/modules/tautulli/modules/activity/widgets/activity_status.dart
@@ -13,6 +13,29 @@ class TautulliActivityStatus extends StatelessWidget {
     @override
     Widget build(BuildContext context) => LSHeader(
         text: '${activity.streamCount} ${activity.streamCount == 1 ? 'Session' : 'Sessions'}',
-        subtitle: 'Total Bandwidth Usage: ${activity.totalBandwidth.lunaKilobytesToString(bytes: false, decimals: 1)}ps',
+        subtitle: [
+            sessions,
+            bandwidth,
+        ].join('\n'),
     );
+
+    String get sessions {
+        String value = 'Sessions: ';
+        value += [
+            if((activity.streamCountDirectPlay ?? 0) > 0) '${activity.streamCountDirectPlay} ${activity.streamCountDirectPlay == 1 ? 'Direct Play' : 'Direct Plays'}',
+            if((activity.streamCountDirectStream ?? 0) > 0) '${activity.streamCountDirectStream} ${activity.streamCountDirectStream == 1 ? 'Direct Stream' : 'Direct Streams'}',
+            if((activity.streamCountTranscode ?? 0) > 0) '${activity.streamCountTranscode} ${activity.streamCountTranscode == 1 ? 'Transcode' : 'Transcodes'}',
+        ].join(', ');
+        return value;
+    }
+
+    String get bandwidth {
+        String value = 'Bandwidth: ${activity.totalBandwidth?.lunaKilobytesToString(bytes: false, decimals: 1) ?? '0.0'}ps (';
+        value += [
+            if((activity.lanBandwidth ?? 0) > 0) 'LAN: ${activity.lanBandwidth.lunaKilobytesToString(bytes: false, decimals: 1)}ps',
+            if((activity.wanBandwidth ?? 0) > 0) 'WAN: ${activity.wanBandwidth.lunaKilobytesToString(bytes: false, decimals: 1)}ps',
+        ].join(', ');
+        value += ')';
+        return value;
+    }
 }

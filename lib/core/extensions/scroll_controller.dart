71137ed1484662f54b050c9159179aad1cc46b4e
Jagandeep Brar <me@jagandeepbrar.io>
2021-02-08 09:37:50 -0600

    [Scroll Controller] Safe-guard luna animate to extension

--- lib/core/extensions/scroll_controller.dart
+++ lib/core/extensions/scroll_controller.dart
@@ -3,9 +3,11 @@ import 'package:lunasea/core.dart';
 
 extension ScrollControllerExtension on ScrollController {
     /// Animate a page back to 1.00, with a predefined duration and curve.
-    Future<void> lunaAnimatedToStart() => this.animateTo(
-        1.00,
-        duration: Duration(milliseconds: LunaUI().uiNavigationSpeed*2),
-        curve: Curves.decelerate,
-    );
+    Future<void> lunaAnimatedToStart() async {
+        if(this.hasClients) this.animateTo(
+            1.00,
+            duration: Duration(milliseconds: LunaUI().uiNavigationSpeed*2),
+            curve: Curves.decelerate,
+        );
+    }
 }

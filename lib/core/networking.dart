e4b7def0faf653033be05d217ef026fdd41084a1
Jagandeep Brar <me@jagandeepbrar.io>
2021-02-09 08:14:15 -0600

    [Networking] Override findProxyFromEnvironment

--- lib/core/networking.dart
+++ lib/core/networking.dart
@@ -15,4 +15,12 @@ class LunaNetworking extends HttpOverrides {
         client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
         return client;
     }
+
+    /// Overrides the default [findProxyFromEnvironment] function.
+    /// 
+    /// Currently just calls the super function to use the default finding function.
+    @override
+    String findProxyFromEnvironment(Uri url, Map<String, String> environment) {
+        return super.findProxyFromEnvironment(url, environment);
+    }
 }

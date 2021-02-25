5e87b1207c641b6c3b88c2f5c7aadc4488838f79
Jagandeep Brar <me@jagandeepbrar.io>
2021-02-25 02:29:45 -0600

    [Notifications] Added Notification Service Extension to allow images in notifications

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Jagandeep Brar on 2021-02-25.
+//  Copyright © 2021 The Chromium Authors. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end

f6740cec86768ba4ed2804bfb7e641548bec5cce
Jagandeep Brar <me@jagandeepbrar.io>
2021-03-03 15:03:33 -0600

    [Lidarr/Webhooks] Deep link to correct location on incoming webhook and notifications

--- lib/modules/lidarr/core/webhooks.dart
+++ lib/modules/lidarr/core/webhooks.dart
@@ -15,23 +15,52 @@ class LidarrWebhooks extends LunaWebhooks {
 }
 
 enum _EventType {
+    DOWNLOAD,
     GRAB,
+    RENAME,
+    RETAG,
+    TEST,
 }
 
 extension _EventTypeExtension on _EventType {
     _EventType fromKey(String key) {
         switch(key) {
+            case 'Download': return _EventType.DOWNLOAD;
             case 'Grab': return _EventType.GRAB;
+            case 'Rename': return _EventType.RENAME;
+            case 'Retag': return _EventType.RETAG;
+            case 'Test': return _EventType.TEST;
         }
         return null;
     }
 
     Future<void> execute(Map<dynamic, dynamic> data) async {
-        // TODO
         switch(this) {
+            case _EventType.DOWNLOAD: return _downloadEvent(data);
             case _EventType.GRAB: return _grabEvent(data);
+            case _EventType.RENAME: return _renameEvent(data);
+            case _EventType.RETAG: return _retagEvent(data);
+            case _EventType.TEST: return _testEvent(data);
         }
     }
 
-    Future<void> _grabEvent(Map<dynamic, dynamic> data) async {}
+    Future<void> _downloadEvent(Map<dynamic, dynamic> data) async {
+        return LunaModule.LIDARR.launch();
+    }
+
+    Future<void> _grabEvent(Map<dynamic, dynamic> data) async {
+        return LunaModule.LIDARR.launch();
+    }
+
+    Future<void> _renameEvent(Map<dynamic, dynamic> data) async {
+        return LunaModule.LIDARR.launch();
+    }
+
+    Future<void> _retagEvent(Map<dynamic, dynamic> data) async {
+        return LunaModule.LIDARR.launch();
+    }
+
+    Future<void> _testEvent(Map<dynamic, dynamic> data) async {
+        return LunaModule.LIDARR.launch();
+    }
 }

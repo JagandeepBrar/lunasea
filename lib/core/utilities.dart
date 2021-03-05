8e7feab9b27ebbeef24f273b6495f94c62854ce9
Jagandeep Brar <me@jagandeepbrar.io>
2021-03-05 14:34:18 -0600

    Added stub localization handler

--- lib/core/utilities.dart
+++ lib/core/utilities.dart
@@ -2,5 +2,6 @@ export 'utilities/changelog.dart';
 export 'utilities/configuration.dart';
 export 'utilities/encryption.dart';
 export 'utilities/filesystem.dart';
+export 'utilities/localization.dart';
 export 'utilities/profile.dart';
 export 'utilities/uuid.dart';

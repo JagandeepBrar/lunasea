4369458ef574e679c26dba13f26f6c8f6d0fa700
Jagandeep Brar <me@jagandeepbrar.io>
2021-03-16 21:49:49 -0500

    [Logger] Abstract Crashlytics general and Flutter error logging, lock to release mode

--- lib/core/utilities/logger.dart
+++ lib/core/utilities/logger.dart
@@ -16,7 +16,7 @@ class LunaLogger {
         LunaFirebaseCrashlytics().setEnabledState();
         FlutterError.onError = (FlutterErrorDetails details, { bool forceReport = false }) async {
             if (kDebugMode) FlutterError.dumpErrorToConsole(details, forceReport: forceReport);
-            LunaFirebaseCrashlytics.instance.recordFlutterError(details);
+            recordCrashlyticsFlutterError(details);
             Zone.current.handleUncaughtError(details.exception, details.stack);
         };
         _compactDatabase();
@@ -53,6 +53,34 @@ class LunaLogger {
     /// Clear all logs currently saved to the database.
     Future<void> clearLogs() async => Database().clearLogsBox();
 
+    /// Records a Crashlytics error if it passes the following checks:
+    /// - In release mode
+    /// - Crashlytics is enabled in the instance
+    /// - Crashlytics is enabled in Hive
+    /// - The error is not a DioError
+    Future<void> recordCrashlyticsError(dynamic error, StackTrace stackTrace) async {
+        if(
+            kReleaseMode &&
+            LunaFirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled &&
+            LunaDatabaseValue.ENABLE_FIREBASE_CRASHLYTICS.data &&
+            !(error is DioError)
+        ) LunaFirebaseCrashlytics.instance.recordError(error, stackTrace);
+    }
+
+    /// Records a Crashlytics flutter error if it passes the following checks:
+    /// - In release mode
+    /// - Crashlytics is enabled in the instance
+    /// - Crashlytics is enabled in Hive
+    /// - The error is not a DioError
+    Future<void> recordCrashlyticsFlutterError(FlutterErrorDetails details) async {
+        if(
+            kReleaseMode &&
+            LunaFirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled &&
+            LunaDatabaseValue.ENABLE_FIREBASE_CRASHLYTICS.data &&
+            !(error is DioError)
+        ) LunaFirebaseCrashlytics.instance.recordFlutterError(details);
+    }
+
     /// Log a new debug-level log.
     void debug(String message) {
         LunaLogHiveObject log =LunaLogHiveObject.withMessage(
@@ -75,7 +103,7 @@ class LunaLogger {
 
     /// Log a new error-level log.
     void error(String message, dynamic error, StackTrace stackTrace) {
-        if(LunaFirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled && !(error is DioError)) LunaFirebaseCrashlytics.instance.recordError(error, stackTrace);
+        recordCrashlyticsError(error, stackTrace);
         LunaLogHiveObject log =LunaLogHiveObject.withError(
             type: LunaLogType.ERROR,
             message: message,
@@ -88,7 +116,7 @@ class LunaLogger {
     /// Log a new critical-level log.
     void critical(dynamic error, StackTrace stackTrace) {
         if(!(error is DioError)) {
-            if(LunaFirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) LunaFirebaseCrashlytics.instance.recordError(error, stackTrace);
+            recordCrashlyticsError(error, stackTrace);
             LunaLogHiveObject log =LunaLogHiveObject.withError(
                 type: LunaLogType.CRITICAL,
                 message: error?.toString() ?? 'Unknown critical error',

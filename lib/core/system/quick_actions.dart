95be949a4374b5e97a9095c754b5d3a9d242efa5
Jagandeep Brar <me@jagandeepbrar.io>
2021-04-08 10:33:56 -0500

    (fix): Further safe-guarded with platform checks

--- lib/core/system/quick_actions.dart
+++ lib/core/system/quick_actions.dart
@@ -1,20 +1,23 @@
+import 'dart:io';
 import 'package:lunasea/core.dart';
 import 'package:quick_actions/quick_actions.dart';
 
 class LunaQuickActions {    
     static final QuickActions _quickActions = QuickActions();
 
+    static bool get isPlatformCompatible => Platform.isAndroid || Platform.isIOS;
+
     /// Initialize the quick actions by setting the action handler.
     /// 
     /// Required before the handler can actually receive any intents.
     void initialize() {
-        _quickActions.initialize((action) => _handler(action));
+        if(isPlatformCompatible) _quickActions.initialize((action) => _handler(action));
         setShortcutItems();
     }
 
     /// Sets the shortcut items by checking the database and enabling the respective action if enabled.
     void setShortcutItems() {
-        _quickActions.setShortcutItems(<ShortcutItem>[
+        if(isPlatformCompatible) _quickActions.setShortcutItems(<ShortcutItem>[
             if(LunaDatabaseValue.QUICK_ACTIONS_SEARCH.data) LunaModule.SEARCH.shortcutItem,
             if(LunaDatabaseValue.QUICK_ACTIONS_LIDARR.data) LunaModule.LIDARR.shortcutItem,
             if(LunaDatabaseValue.QUICK_ACTIONS_RADARR.data) LunaModule.RADARR.shortcutItem,

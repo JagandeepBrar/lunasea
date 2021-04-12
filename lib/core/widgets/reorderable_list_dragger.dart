e7b164771e2f5e7efa8e3d170122e0fd3b98f76c
Jagandeep Brar <me@jagandeepbrar.io>
2021-04-12 05:41:07 -0500

    Add LunaReorderableListDragger widget for handler

---
+++ lib/core/widgets/reorderable_list_dragger.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LunaReorderableListDragger extends StatelessWidget {
+    final int index;
+
+    LunaReorderableListDragger({
+        Key key,
+        @required this.index,
+    }) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                ReorderableDragStartListener(
+                    index: index,
+                    child: Icon(Icons.menu_rounded),
+                ),
+            ],
+        );
+    }
+}

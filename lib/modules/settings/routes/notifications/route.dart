bfc5793358654015b402b4f18d8886f072f5dcaf
Jagandeep Brar <me@jagandeepbrar.io>
2021-04-14 19:13:33 -0500

    Added stub notifications route in settings

---
+++ lib/modules/settings/routes/notifications/route.dart
@@ -0,0 +1,48 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+import 'package:lunasea/core.dart';
+import 'package:lunasea/modules/settings.dart';
+
+class SettingsNotificationsRouter extends SettingsPageRouter {
+    SettingsNotificationsRouter() : super('/settings/notifications');
+
+    @override
+    void defineRoute(FluroRouter router) => super.noParameterRouteDefinition(router, _SettingsNotificationsRoute());
+}
+
+class _SettingsNotificationsRoute extends StatefulWidget {
+    @override
+    State<_SettingsNotificationsRoute> createState() => _State();
+}
+
+class _State extends State<_SettingsNotificationsRoute> with LunaScrollControllerMixin {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    @override
+    Widget build(BuildContext context) {
+        return  LunaScaffold(
+            scaffoldKey: _scaffoldKey,
+            appBar: _appBar(),
+            body: _body(),
+        );
+    }
+
+    Widget _appBar() {
+        return LunaAppBar(
+            title: 'Notifications',
+            scrollControllers: [scrollController],
+        );
+    }
+
+    Widget _body() {
+        return LunaListView(
+            controller: scrollController,
+            children: [
+                LunaBanner(
+                    headerText: 'Beta',
+                    bodyText: 'Webhook-based notifications are currently in beta, and are only available for the modules listed below.\n\nAdditional module support will come in the future!',
+                ),
+            ],
+        );
+    }
+}

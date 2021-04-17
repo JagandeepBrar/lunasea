80545fb8a29009d5657b563d25c7de84ef879e9a
Jagandeep Brar <me@jagandeepbrar.io>
2021-04-17 06:24:39 -0500

    fix(packages): Switch from share to share_plus package for extended platform support

--- lib/core/system/filesystem.dart
+++ lib/core/system/filesystem.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 import 'package:lunasea/core.dart';
-import 'package:share/share.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 class LunaFileSystem {

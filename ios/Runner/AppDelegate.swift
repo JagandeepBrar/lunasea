cbd55d5418b05ded4839899291b359e0c7e5f7d9
Jagandeep Brar <me@jagandeepbrar.io>
2021-04-19 21:45:11 -0500

    chore(packages): Updated packages, removed manual registration of quick_actions delegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,14 +13,4 @@ import Flutter
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
-
-    @available(iOS 9.0, *)
-    override func application(
-        _ application: UIApplication,
-        performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void
-    ) {
-        let controller = window.rootViewController as? FlutterViewController
-        let channel = FlutterMethodChannel(name: "plugins.flutter.io/quick_actions", binaryMessenger: controller! as! FlutterBinaryMessenger)
-        channel.invokeMethod("launch", arguments: shortcutItem.type)
-    }
 }

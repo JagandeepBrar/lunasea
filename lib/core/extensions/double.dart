13edb8822f5b52673f05ae461765d6eabb7502b6
Jagandeep Brar <me@jagandeepbrar.io>
2021-05-03 04:35:05 -0500

    feat(tests): Add tests for core/extensions/double.dart

--- lib/core/extensions/double.dart
+++ lib/core/extensions/double.dart
@@ -5,7 +5,9 @@ extension DoubleExtension on double {
   ///
   /// Time greater than 48 hours => "x Days Ago" else "x Hours Ago"
   String lunaHoursToAge() {
-    if (this == null) return LunaUI.TEXT_EMDASH;
+    if (this == null || this < 0) return LunaUI.TEXT_EMDASH;
+    // Small buffer of 5% of an hour, 3 minute
+    if (this <= 0.05) return 'Just Now';
     int days = (this / 24).floor();
     if (this > 48) return '$days Days Ago';
     return this == 1 ? '1 Hour Ago' : '${this.toStringAsFixed(1)} Hours Ago';

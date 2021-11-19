018d89db9d3819cb87da77ce164ba6f8621e2ea8
Jagandeep Brar <me@jagandeepbrar.io>
2021-11-19 08:48:25 -0500

    fix(modal sheet): Grey screen on some sheets

--- lib/core/ui/bottom_modal_sheet.dart
+++ lib/core/ui/bottom_modal_sheet.dart
@@ -5,17 +5,18 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 class LunaBottomModalSheet {
   Future<dynamic> showModal({
     @required BuildContext context,
-    Widget Function(BuildContext) builder,
-  }) async =>
-      showBarModalBottomSheet(
-        context: context,
-        expand: false,
-        backgroundColor:
-            LunaTheme.isAMOLEDTheme ? Colors.black : LunaColours.secondary,
-        shape: LunaUI.shapeBorder,
-        builder: this.builder ?? builder,
-        closeProgressThreshold: 0.90,
-      );
+    Widget Function(BuildContext context) builder,
+  }) async {
+    return showBarModalBottomSheet(
+      context: context,
+      expand: false,
+      backgroundColor:
+          LunaTheme.isAMOLEDTheme ? Colors.black : LunaColours.secondary,
+      shape: LunaUI.shapeBorder,
+      builder: builder ?? this.builder,
+      closeProgressThreshold: 0.90,
+    );
+  }
 
   Widget builder(BuildContext context) => null;
 }

420d11c37188fe1d56d76b51b98325b384a45cff
Jagandeep Brar <me@jagandeepbrar.io>
2022-02-11 13:30:09 -0500

    feat(http): set User-Agent to "LunaSea/{version} {build}"

--- lib/core/system/networking.dart
+++ lib/core/system/networking.dart
@@ -1,11 +1,16 @@
 import 'dart:io';
 
 import 'package:lunasea/core.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class LunaNetworking extends HttpOverrides {
   /// Initialize the default HTTP client by setting [HttpOverrides]'s global HTTP client to [LunaNetworking].
   void initialize() {
     HttpOverrides.global = LunaNetworking();
+  }
+
+  String generateUserAgent(PackageInfo info) {
+    return '${info.appName}/${info.version} ${info.buildNumber}';
   }
 
   /// Overrides the default [createHttpClient] function and ensures all HTTPS connections allow bad certificate connections.
@@ -14,8 +19,16 @@ class LunaNetworking extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
     final HttpClient client = super.createHttpClient(context);
+
+    // Disable TLS validation
     if (!LunaDatabaseValue.NETWORKING_TLS_VALIDATION.data)
       client.badCertificateCallback = (cert, host, port) => true;
+
+    // Set User-Agent
+    PackageInfo.fromPlatform()
+        .then((info) => client.userAgent = generateUserAgent(info))
+        .catchError((_) => client.userAgent = 'LunaSea/Unknown');
+
     return client;
   }
 

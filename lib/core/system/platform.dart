43cb7a610640b6f6d713cdb2ec153d5999a3db9a
Jagandeep Brar <me@jagandeepbrar.io>
2022-03-20 16:29:51 -0400

    fix(web): set notification vapid key

--- lib/core/system/platform.dart
+++ lib/core/system/platform.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/foundation.dart';
 
 class LunaPlatform {
-  bool get isAndroid => defaultTargetPlatform == TargetPlatform.android;
-  bool get isIOS => defaultTargetPlatform == TargetPlatform.iOS;
-
-  bool get isLinux => defaultTargetPlatform == TargetPlatform.linux;
-  bool get isMacOS => defaultTargetPlatform == TargetPlatform.macOS;
-  bool get isWindows => defaultTargetPlatform == TargetPlatform.windows;
-
   bool get isWeb => kIsWeb;
-
   bool get isMobile => isAndroid || isIOS;
   bool get isDesktop => isLinux || isMacOS || isWindows;
+
+  bool get isAndroid {
+    return !kIsWeb && defaultTargetPlatform == TargetPlatform.android;
+  }
+
+  bool get isIOS {
+    return !kIsWeb && defaultTargetPlatform == TargetPlatform.iOS;
+  }
+
+  bool get isLinux {
+    return !kIsWeb && defaultTargetPlatform == TargetPlatform.linux;
+  }
+
+  bool get isMacOS {
+    return !kIsWeb && defaultTargetPlatform == TargetPlatform.macOS;
+  }
+
+  bool get isWindows {
+    return !kIsWeb && defaultTargetPlatform == TargetPlatform.windows;
+  }
 }

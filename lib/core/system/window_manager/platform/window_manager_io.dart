d6aed6c707ad75c0a02c3a4fa496ee6d8adeb55e
Jagandeep Brar <me@jagandeepbrar.io>
2022-03-24 00:23:47 -0400

    feat(window_manager): rewrite window manager to be platform-safe

---
+++ lib/core/system/window_manager/platform/window_manager_io.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+
+import '../../platform.dart';
+import '../window_manager.dart';
+
+bool isPlatformSupported() => LunaPlatform().isDesktop;
+LunaWindowManager getWindowManager() {
+  switch (defaultTargetPlatform) {
+    case TargetPlatform.linux:
+    case TargetPlatform.macOS:
+    case TargetPlatform.windows:
+      return IO();
+    default:
+      throw UnsupportedError('LunaWindowManager unsupported');
+  }
+}
+
+class IO implements LunaWindowManager {
+  @override
+  Future<void> initialize() async {
+    await windowManager.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      const Size minSize = Size(
+        LunaWindowManager.MINIMUM_WINDOW_SIZE,
+        LunaWindowManager.MINIMUM_WINDOW_SIZE,
+      );
+      const Size size = Size(
+        LunaWindowManager.INITIAL_WINDOW_SIZE,
+        LunaWindowManager.INITIAL_WINDOW_SIZE,
+      );
+
+      if (!kDebugMode) await windowManager.setSize(size);
+      await windowManager.setMinimumSize(minSize);
+      setWindowTitle('LunaSea');
+
+      windowManager.show();
+    });
+  }
+
+  @override
+  Future<void> setWindowTitle(String title) async {
+    return windowManager
+        .waitUntilReadyToShow()
+        .then((_) async => await windowManager.setTitle(title));
+  }
+}
---
+++ lib/core/system/window_manager/window_manager.dart
@@ -0,0 +1,14 @@
+import 'platform/window_manager_stub.dart'
+    if (dart.library.io) 'platform/window_manager_io.dart'
+    if (dart.library.html) 'platform/window_manager_html.dart';
+
+abstract class LunaWindowManager {
+  static const double MINIMUM_WINDOW_SIZE = 400;
+  static const double INITIAL_WINDOW_SIZE = 700;
+
+  static bool get isSupported => isPlatformSupported();
+  factory LunaWindowManager() => getWindowManager();
+
+  Future<void> initialize();
+  Future<void> setWindowTitle(String title);
+}

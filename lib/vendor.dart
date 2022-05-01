642e96c1eaa927ec943b520cff0d77c31e6c3eb6
Jagandeep Brar <me@jagandeepbrar.io>
2022-04-30 22:55:07 -0400

    chore(state): add support for Riverpod state management [skip ci]

--- lib/vendor.dart
+++ lib/vendor.dart
@@ -1,5 +1,5 @@
 // Dart
-export 'dart:async';
+export 'dart:async' hide AsyncError;
 export 'dart:convert';
 export 'dart:math';
 export 'dart:typed_data';
@@ -9,7 +9,6 @@ export 'package:easy_localization/easy_localization.dart';
 export 'package:expandable/expandable.dart';
 export 'package:fading_edge_scrollview/fading_edge_scrollview.dart';
 export 'package:flash/flash.dart';
-export 'package:fluro/fluro.dart';
 export 'package:go_router/go_router.dart';
 export 'package:hive/hive.dart';
 export 'package:hive_flutter/hive_flutter.dart';
@@ -17,7 +16,6 @@ export 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:layout/layout.dart';
 export 'package:package_info_plus/package_info_plus.dart';
-export 'package:provider/provider.dart';
 export 'package:simple_gesture_detector/simple_gesture_detector.dart';
 export 'package:stash/stash_api.dart';
 export 'package:stash_memory/stash_memory.dart';
@@ -26,3 +24,6 @@ export 'package:table_calendar/table_calendar.dart';
 export 'package:transparent_image/transparent_image.dart';
 export 'package:tuple/tuple.dart';
 export 'package:xml/xml.dart';
+// Deprecated - Should eventually be removed
+export 'package:fluro/fluro.dart';
+export 'package:provider/provider.dart';

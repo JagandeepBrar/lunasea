f3931c6b08025a70920039e5cbbc9cf7120c0b36
Jagandeep Brar <me@jagandeepbrar.io>
2022-09-26 12:54:14 -0400

    fix(backup): edge-case where backup/restore would fail

--- lib/database/tables/bios.dart
+++ lib/database/tables/bios.dart
@@ -13,4 +13,32 @@ enum BIOSDatabase<T> with LunaTableMixin<T> {
   final T fallback;
 
   const BIOSDatabase(this.fallback);
+
+  @override
+  dynamic export() {
+    BIOSDatabase db = this;
+    switch (db) {
+      case BIOSDatabase.BOOT_MODULE:
+        return BIOSDatabase.BOOT_MODULE.read().key;
+      default:
+        return super.export();
+    }
+  }
+
+  @override
+  void import(dynamic value) {
+    BIOSDatabase db = this;
+    dynamic result;
+
+    switch (db) {
+      case BIOSDatabase.BOOT_MODULE:
+        result = LunaModule.fromKey(value.toString());
+        break;
+      default:
+        result = value;
+        break;
+    }
+
+    return super.import(result);
+  }
 }

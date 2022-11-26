753e7f0e984b54df56cc803f9dfc764abb2558f0
Jagandeep Brar <me@jagandeepbrar.io>
2022-11-26 18:28:26 -0500

    feat(linux): include MD5 sums in Debian distributions

--- scripts/generate_debian.dart
+++ scripts/generate_debian.dart
@@ -1,73 +1,55 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  _createSkeleton();
-
-  _generateControl(args[0]);
-  _generateDesktopEntry();
-  _copyIcon();
+  _setVersion(args[0]);
   _copyBuild();
-
+  _determineMD5Sums();
   _buildDebian();
 }
 
-Future<void> _createSkeleton() async {
-  if (Directory('debian').existsSync()) {
-    Directory('debian').deleteSync(recursive: true);
-  }
-
-  Directory('debian/DEBIAN').createSync(recursive: true);
-  Directory('debian/usr/local/lib').createSync(recursive: true);
-  Directory('debian/usr/share/applications').createSync(recursive: true);
-  Directory('debian/usr/share/icons').createSync(recursive: true);
-}
-
-void _generateControl(String version) {
+void _setVersion(String version) {
   final control = File('debian/DEBIAN/control');
-
-  String data = '';
-  data += 'Version:$version\n';
-  data += 'Architecture:amd64\n';
-  data += 'Package:LunaSea\n';
-  data += 'Essential:no\n';
-  data += 'Priority:optional\n';
-  data += 'Maintainer:LunaSea Support <hello@lunasea.app>\n';
-  data += 'Description:Self-hosted software controller built using Flutter\n';
-
-  control.writeAsStringSync(data);
-}
-
-void _generateDesktopEntry() {
-  final entry = File('debian/usr/share/applications/lunasea.desktop');
-
-  String data = '';
-  data += '[Desktop Entry]\n';
-  data += 'Name=LunaSea\n';
-  data += 'Comment=Self-hosted software controller built using Flutter\n';
-  data += 'Icon=/usr/share/icons/lunasea.png\n';
-  data += 'Terminal=false\n';
-  data += 'Type=Application\n';
-  data += 'Categories=Utilities;Entertainment;\n';
-  data += 'Exec=/usr/local/lib/LunaSea/lunasea\n';
-  data += 'TryExec=/usr/local/lib/LunaSea/lunasea\n';
-
-  entry.writeAsStringSync(data);
-}
-
-void _copyIcon() {
-  final icon = File('assets/icon/icon_linux.png');
-  icon.copySync('debian/usr/share/icons/lunasea.png');
+  control.writeAsStringSync('Version:$version\n', mode: FileMode.append);
 }
 
 void _copyBuild() {
-  Process.runSync(
-    'cp',
-    ['-r', 'build/linux/x64/release/bundle', 'debian/usr/local/lib'],
-  );
-  Process.runSync(
-    'mv',
-    ['debian/usr/local/lib/bundle', 'debian/usr/local/lib/LunaSea'],
-  );
+  const path = 'debian/usr/local/lib';
+
+  final directory = Directory(path);
+  if (directory.existsSync()) directory.deleteSync(recursive: true);
+  directory.createSync(recursive: true);
+
+  Process.runSync('cp', [
+    '-r',
+    'build/linux/x64/release/bundle',
+    path,
+  ]);
+  Process.runSync('mv', [
+    '$path/bundle',
+    '$path/LunaSea',
+  ]);
+}
+
+void _determineMD5Sums() {
+  final find = Process.runSync(
+    'find',
+    [".", "-type", "f", "-not", "-path", "'./DEBIAN/*'"],
+    workingDirectory: 'debian',
+  ).stdout as String;
+
+  final files = find
+      .split('\n')
+      .where((file) => !file.startsWith('./DEBIAN'))
+      .map((file) => file.replaceAll('./', ''))
+      .toList();
+
+  final md5 = Process.runSync(
+    'md5sum',
+    files,
+    workingDirectory: 'debian',
+  ).stdout as String;
+
+  File('debian/DEBIAN/md5sums').writeAsStringSync(md5);
 }
 
 void _buildDebian() {
@@ -75,10 +57,10 @@ void _buildDebian() {
     Directory('output').createSync(recursive: true);
   }
 
-  Process.runSync(
-    'dpkg-deb',
-    ['--build', 'debian'],
-  );
+  Process.runSync('dpkg-deb', [
+    '--build',
+    'debian',
+  ]);
   Process.runSync('mv', [
     'debian.deb',
     'output/lunasea-linux-amd64.deb',

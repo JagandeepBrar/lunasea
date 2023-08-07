f8573bc38cf140df39cea9edbd94084ee2bc06d2
The Crimson Chin <yourpocketlint@gmail.com>
2023-08-07 14:03:03 -0500

    Add files via upload

---
+++ lib/modules/mylar/core.dart
@@ -0,0 +1,7 @@
+export 'package:lunasea/database/tables/sonarr.dart';
+export 'core/api_controller.dart';
+export 'core/dialogs.dart';
+export 'core/extensions.dart';
+export 'core/state.dart';
+export 'core/types.dart';
+export 'core/webhooks.dart';
---
+++ lib/modules/mylar/routes.dart
@@ -0,0 +1,14 @@
+export 'routes/add_series.dart';
+export 'routes/add_series_details.dart';
+export 'routes/catalogue.dart';
+export 'routes/edit_series.dart';
+export 'routes/history.dart';
+export 'routes/missing.dart';
+export 'routes/more.dart';
+export 'routes/queue.dart';
+export 'routes/releases.dart';
+export 'routes/season_details.dart';
+export 'routes/series_details.dart';
+export 'routes/sonarr.dart';
+export 'routes/tags.dart';
+export 'routes/upcoming.dart';

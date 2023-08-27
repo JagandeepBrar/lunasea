ffc9d98cd2e7af7430dc534b1c9aae98cbbb844d
Jagandeep Brar <me@jagandeepbrar.io>
2023-08-27 14:26:00 -0400

    fix(ui): upgrade to flash@3.0.5+1

--- lib/widgets/ui/snackbar/snackbar.dart
+++ lib/widgets/ui/snackbar/snackbar.dart
@@ -45,64 +45,62 @@ Future<void> showLunaSnackBar({
   bool showButton = false,
   String buttonText = 'view',
   Function? buttonOnPressed,
-}) async =>
-    showFlash(
-      context: LunaState.context,
-      duration: duration ?? Duration(seconds: showButton ? 4 : 2),
-      builder: (context, controller) => Flash(
-        backgroundColor: Theme.of(context).primaryColor,
-        controller: controller,
-        boxShadows: [
-          BoxShadow(
-            blurRadius: LunaUI.DEFAULT_MARGIN_SIZE / 2,
-            spreadRadius: LunaUI.DEFAULT_MARGIN_SIZE / 4,
-            color: Colors.black.withOpacity(LunaUI.OPACITY_SPLASH / 2),
-          ),
-        ],
-        horizontalDismissDirection: HorizontalDismissDirection.horizontal,
-        margin: LunaUI.MARGIN_DEFAULT,
-        behavior: FlashBehavior.floating,
-        position: position,
+}) async {
+  showFlash(
+    context: LunaState.context,
+    duration: duration ?? Duration(seconds: showButton ? 4 : 2),
+    transitionDuration: const Duration(milliseconds: LunaUI.ANIMATION_SPEED),
+    reverseTransitionDuration:
+        const Duration(milliseconds: LunaUI.ANIMATION_SPEED),
+    builder: (context, controller) => FlashBar(
+      controller: controller,
+      backgroundColor: Theme.of(context).primaryColor,
+      behavior: FlashBehavior.floating,
+      margin: LunaUI.MARGIN_DEFAULT,
+      position: position,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color:
+              LunaUI.shouldUseBorder ? LunaColours.white10 : Colors.transparent,
+        ),
         borderRadius: BorderRadius.circular(LunaUI.BORDER_RADIUS),
-        borderColor:
-            LunaUI.shouldUseBorder ? LunaColours.white10 : Colors.transparent,
-        child: FlashBar(
-          title: LunaText.title(
-            text: title,
-            maxLines: 4,
-            overflow: TextOverflow.ellipsis,
-          ),
-          content: LunaText.subtitle(
-            text: message,
-            maxLines: 8,
-            overflow: TextOverflow.ellipsis,
-          ),
-          shouldIconPulse: false,
-          icon: Padding(
-            child: LunaIconButton(
-              icon: type.icon,
-              color: type.color,
-            ),
-            padding: const EdgeInsets.only(
-              left: LunaUI.DEFAULT_MARGIN_SIZE / 2,
-            ),
-          ),
-          primaryAction: showButton
-              ? TextButton(
-                  child: Text(
-                    buttonText.toUpperCase(),
-                    style: const TextStyle(
-                      fontWeight: LunaUI.FONT_WEIGHT_BOLD,
-                      color: LunaColours.accent,
-                    ),
-                  ),
-                  onPressed: () {
-                    HapticFeedback.lightImpact();
-                    controller.dismiss();
-                    buttonOnPressed!();
-                  },
-                )
-              : null,
+      ),
+      title: LunaText.title(
+        text: title,
+        maxLines: 4,
+        overflow: TextOverflow.ellipsis,
+      ),
+      content: LunaText.subtitle(
+        text: message,
+        maxLines: 8,
+        overflow: TextOverflow.ellipsis,
+      ),
+      shouldIconPulse: false,
+      icon: Padding(
+        child: LunaIconButton(
+          icon: type.icon,
+          color: type.color,
+        ),
+        padding: const EdgeInsets.only(
+          left: LunaUI.DEFAULT_MARGIN_SIZE / 2,
         ),
       ),
-    );
+      primaryAction: showButton
+          ? TextButton(
+              child: Text(
+                buttonText.toUpperCase(),
+                style: const TextStyle(
+                  fontWeight: LunaUI.FONT_WEIGHT_BOLD,
+                  color: LunaColours.accent,
+                ),
+              ),
+              onPressed: () {
+                HapticFeedback.lightImpact();
+                controller.dismiss();
+                buttonOnPressed!();
+              },
+            )
+          : null,
+    ),
+  );
+}
